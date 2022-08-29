88d27453377bd931dba34b0dfc3a946ea6b65150
Evangelos Vandoros <vandvag@gmail.com>
2022-08-29 16:55:28 +0300

    added utility functions

--- utils/Utilities.cpp
+++ utils/Utilities.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "Utilities.h"
+
+std::vector<std::string> Utilities::readLinesFromFile(const std::filesystem::path& path) {
+	// ensure that path exists
+	verifyElseCrash(std::filesystem::exists(path));
+	std::ifstream fileStream(path);
+	std::string currentLine;
+	std::vector<std::string> allLines;
+
+	while (std::getline(fileStream, currentLine)) {
+		allLines.push_back(currentLine);
+	}
+	return allLines;
+}
+
+void Utilities::verifyElseCrash(bool bAssert) {
+	assert(bAssert);
+
+	if (!bAssert) {
+		std::terminate();
+	}
+}
+
+std::vector<std::string> Utilities::splitString(const std::string& input, const std::string& delimiter) {
+	std::vector<std::string> tokens;
+	size_t nextTokenStart = 0;
+	while (nextTokenStart < input.size()) {
+		size_t delimiterStart = input.find(delimiter, nextTokenStart);
+		if (delimiterStart == std::string::npos) {
+			delimiterStart = input.size();
+		}
+		std::string token = input.substr(nextTokenStart, delimiterStart - nextTokenStart);
+		tokens.push_back(token);
+
+		nextTokenStart = delimiterStart + delimiter.size();
+	}
+	return tokens;
+}
+
--- utils/Utilities.h
+++ utils/Utilities.h
@@ -2,7 +2,19 @@
 #ifndef ADVENT_CPP_2020_UTILITIES_H
 #define ADVENT_CPP_2020_UTILITIES_H
 
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <cassert>
+
 class Utilities {
+	// Read all lines from an input file and store them in a vector
+	static std::vector<std::string> readLinesFromFile(const std::filesystem::path& path);
+	// Utility function that crashes the program if something unexpected happened
+	static void verifyElseCrash(bool bAssert);
+	// Split a string based on a delimiter (because for some reason standard library doesn't have an implementation)
+	static std::vector<std::string> splitString(const std::string& input, const std::string& delimiter)
 
 };
 
