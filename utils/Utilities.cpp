be1b1805043d96856979e0a8d90ca567a14c339c
Evangelos Vandoros <vandvag@gmail.com>
2022-08-29 16:34:31 +0300

    Initial commit

---
+++ utils/Utilities.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evangelos Vandoros on 29/8/22.
+//
+
+#include "Utilities.h"
---
+++ utils/Utilities.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef ADVENT_CPP_2020_UTILITIES_H
+#define ADVENT_CPP_2020_UTILITIES_H
+
+class Utilities {
+
+};
+
+#endif //ADVENT_CPP_2020_UTILITIES_H
