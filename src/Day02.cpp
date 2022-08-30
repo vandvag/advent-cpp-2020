38551417679155fe7194ab1f8191deb3d123ac12
Evangelos Vandoros <vandvag@gmail.com>
2022-08-30 11:06:09 +0300

    added files for day2

---
+++ src/Day02.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Evangelos Vandoros on 30/8/22.
+//
+
+#include "Day02.h"
+
+Day02::Day02() :input(Utilities::readLinesFromFile("../inputs/day02-sample.in")){
+
+}
+
+void Day02::solutionPart1() {
+	std::cout << "Solution for Day 2, Part 1:" << "\n";
+}
+
+void Day02::solutionPart2() {
+	std::cout << "Solution for Day 2, Part 2:" << "\n";
+}
---
+++ src/Day02.h
@@ -0,0 +1,25 @@
+#pragma once
+//
+// Created by Evangelos Vandoros on 30/8/22.
+//
+
+#ifndef ADVENT_CPP_2020_DAY02_H
+#define ADVENT_CPP_2020_DAY02_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "../utils/Utilities.h"
+
+class Day02 {
+
+private:
+	std::vector<std::string> input;
+public:
+	Day02();
+	void solutionPart1();
+	void solutionPart2();
+};
+
+#endif //ADVENT_CPP_2020_DAY02_H
